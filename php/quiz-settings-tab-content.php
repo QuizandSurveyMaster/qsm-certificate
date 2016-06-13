e3b04f89216d29f215bba716655066b8db0b1982
Frank Corso <frank@mylocalwebstop.com>
2016-06-12 22:07:24 -0400

    Adds explanation text to quiz settings tab

--- php/quiz-settings-tab-content.php
+++ php/quiz-settings-tab-content.php
@@ -72,65 +72,64 @@ function qsm_addon_certificate_quiz_settings_tabs_content() {
   $certificate_settings = wp_parse_args( $certificate_settings, $certificate_defaults );
 
 	?>
-	<div id="tabs-5" class="mlw_tab_content">
-    <h2>Certificate</h2>
-    <form action="" method="post">
-      <button class="button-primary">Save Settings</button>
-  		<table class="form-table">
-  			<tr valign="top">
-  				<td><label for="enableCertificates">Enable certificates for this quiz/survey?</label></td>
-  				<td>
-  				    <input type="radio" id="radio30" name="enableCertificates" <?php checked( $certificate_settings["enabled"], '0' ); ?> value='0' /><label for="radio30">Yes</label><br>
-  				    <input type="radio" id="radio31" name="enableCertificates" <?php checked( $certificate_settings["enabled"], '1' ); ?> value='1' /><label for="radio31">No</label><br>
-  				</td>
-  			</tr>
-  			<tr>
-  				<td width="30%">
-  					<strong>Title</strong>
-  				</td>
-  				<td><textarea cols="80" rows="15" id="certificate_title" name="certificate_title"><?php echo $certificate_settings["title"]; ?></textarea>
-  				</td>
-  			</tr>
-  			<tr>
-  				<td width="30%">
-  					<strong>Content</strong>
-  					<br />
-  					<p><?php _e('Allowed Variables:', 'quiz-master-next'); ?></p>
-  					<p style="margin: 2px 0">- %POINT_SCORE%</p>
-  					<p style="margin: 2px 0">- %AVERAGE_POINT%</p>
-  					<p style="margin: 2px 0">- %AMOUNT_CORRECT%</p>
-  					<p style="margin: 2px 0">- %TOTAL_QUESTIONS%</p>
-  					<p style="margin: 2px 0">- %CORRECT_SCORE%</p>
-  					<p style="margin: 2px 0">- %QUIZ_NAME%</p>
-  					<p style="margin: 2px 0">- %USER_NAME%</p>
-  					<p style="margin: 2px 0">- %USER_BUSINESS%</p>
-  					<p style="margin: 2px 0">- %USER_PHONE%</p>
-  					<p style="margin: 2px 0">- %USER_EMAIL%</p>
-  					<p style="margin: 2px 0">- %CURRENT_DATE%</p>
-  					<p style="margin: 2px 0">- %DATE_TAKEN%</p>
-  				</td>
-  				<td><label for="certificate_template">Allowed tags: &lt;b&gt; - bold, &lt;i&gt;-italics, &lt;u&gt;-underline, &lt;br&gt;-New Line or start a new line by pressing enter</label><textarea cols="80" rows="15" id="certificate_template" name="certificate_template"><?php echo $certificate_settings["content"]; ?></textarea>
-  				</td>
-  			</tr>
-  			<tr>
-  				<td width="30%">
-  					<strong>URL To Logo (Must be JPG, JPEG, PNG or GIF)</strong>
-  				</td>
-  				<td><textarea cols="80" rows="15" id="certificate_logo" name="certificate_logo"><?php echo $certificate_settings["logo"]; ?></textarea>
-  				</td>
-  			</tr>
-  			<tr>
-  				<td width="30%">
-  					<strong>URL To Background Img (Must be JPG, JPEG, PNG or GIF)</strong>
-  				</td>
-  				<td><textarea cols="80" rows="15" id="certificate_background" name="certificate_background"><?php echo $certificate_settings["background"]; ?></textarea>
-  				</td>
-  			</tr>
-  		</table>
-      <?php wp_nonce_field('certificate','certificate_nonce'); ?>
-  		<button class="button-primary">Save Settings</button>
-		</form>
-	</div>
+  <h2>Certificate</h2>
+  <p>After enabling and configuring your certificate, you will have to add it to an email on the Emails tab or a results page on the Results Page tab using the %CERTIFICATE_LINK% variable.</p>
+  <form action="" method="post">
+    <button class="button-primary">Save Settings</button>
+		<table class="form-table">
+			<tr valign="top">
+				<td><label for="enableCertificates">Enable certificates for this quiz/survey?</label></td>
+				<td>
+				    <input type="radio" id="radio30" name="enableCertificates" <?php checked( $certificate_settings["enabled"], '0' ); ?> value='0' /><label for="radio30">Yes</label><br>
+				    <input type="radio" id="radio31" name="enableCertificates" <?php checked( $certificate_settings["enabled"], '1' ); ?> value='1' /><label for="radio31">No</label><br>
+				</td>
+			</tr>
+			<tr>
+				<td width="30%">
+					<strong>Title</strong>
+				</td>
+				<td><textarea cols="80" rows="15" id="certificate_title" name="certificate_title"><?php echo $certificate_settings["title"]; ?></textarea>
+				</td>
+			</tr>
+			<tr>
+				<td width="30%">
+					<strong>Content</strong>
+					<br />
+					<p><?php _e('Allowed Variables:', 'quiz-master-next'); ?></p>
+					<p style="margin: 2px 0">- %POINT_SCORE%</p>
+					<p style="margin: 2px 0">- %AVERAGE_POINT%</p>
+					<p style="margin: 2px 0">- %AMOUNT_CORRECT%</p>
+					<p style="margin: 2px 0">- %TOTAL_QUESTIONS%</p>
+					<p style="margin: 2px 0">- %CORRECT_SCORE%</p>
+					<p style="margin: 2px 0">- %QUIZ_NAME%</p>
+					<p style="margin: 2px 0">- %USER_NAME%</p>
+					<p style="margin: 2px 0">- %USER_BUSINESS%</p>
+					<p style="margin: 2px 0">- %USER_PHONE%</p>
+					<p style="margin: 2px 0">- %USER_EMAIL%</p>
+					<p style="margin: 2px 0">- %CURRENT_DATE%</p>
+					<p style="margin: 2px 0">- %DATE_TAKEN%</p>
+				</td>
+				<td><label for="certificate_template">Allowed tags: &lt;b&gt; - bold, &lt;i&gt;-italics, &lt;u&gt;-underline, &lt;br&gt;-New Line or start a new line by pressing enter</label><textarea cols="80" rows="15" id="certificate_template" name="certificate_template"><?php echo $certificate_settings["content"]; ?></textarea>
+				</td>
+			</tr>
+			<tr>
+				<td width="30%">
+					<strong>URL To Logo (Must be JPG, JPEG, PNG or GIF)</strong>
+				</td>
+				<td><textarea cols="80" rows="15" id="certificate_logo" name="certificate_logo"><?php echo $certificate_settings["logo"]; ?></textarea>
+				</td>
+			</tr>
+			<tr>
+				<td width="30%">
+					<strong>URL To Background Img (Must be JPG, JPEG, PNG or GIF)</strong>
+				</td>
+				<td><textarea cols="80" rows="15" id="certificate_background" name="certificate_background"><?php echo $certificate_settings["background"]; ?></textarea>
+				</td>
+			</tr>
+		</table>
+    <?php wp_nonce_field('certificate','certificate_nonce'); ?>
+		<button class="button-primary">Save Settings</button>
+	</form>
   <?php
 }
 ?>

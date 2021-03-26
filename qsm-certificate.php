919137083265c4215cb8b17b87bce7e525d378bc
Aditya Jain <aditya@expresstech.io>
2021-03-26 12:12:21 +0530

    Release 1.0.10

--- qsm-certificate.php
+++ qsm-certificate.php
@@ -5,10 +5,10 @@
  * Description: Adds the ability to give certificates to quiz/survey takers
  * Author: QSM Team
  * Author URI: http://quizandsurveymaster.com
- * Version: 1.0.9
+ * Version: 1.0.10
  *
  * @author
- * @version 1.0.9
+ * @version 1.0.10
  */
 
 if ( ! defined( 'ABSPATH' ) ) exit;
@@ -29,7 +29,7 @@ class QSM_Certificate {
   	 * @var string
   	 * @since 0.1.0
   	 */
-  	public $version = '1.0.9';
+  	public $version = '1.0.10';
 
     /**
   	  * Main Construct Function

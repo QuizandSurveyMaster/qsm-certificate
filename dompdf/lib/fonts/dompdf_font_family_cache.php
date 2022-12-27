9be6f55756b69635a6f8219440acc95e86897c00
Vibina <vibina@expresstech.io>
2022-12-27 15:41:59 +0530

    changed the library to dompdf

---
+++ dompdf/lib/fonts/dompdf_font_family_cache.php
@@ -0,0 +1,93 @@
+<?php return function ($fontDir, $rootDir) {
+return array (
+  'sans-serif' => array(
+    'normal' => $fontDir . '/Helvetica',
+    'bold' => $fontDir . '/Helvetica-Bold',
+    'italic' => $fontDir . '/Helvetica-Oblique',
+    'bold_italic' => $fontDir . '/Helvetica-BoldOblique',
+  ),
+  'times' => array(
+    'normal' => $fontDir . '/Times-Roman',
+    'bold' => $fontDir . '/Times-Bold',
+    'italic' => $fontDir . '/Times-Italic',
+    'bold_italic' => $fontDir . '/Times-BoldItalic',
+  ),
+  'times-roman' => array(
+    'normal' => $fontDir . '/Times-Roman',
+    'bold' => $fontDir . '/Times-Bold',
+    'italic' => $fontDir . '/Times-Italic',
+    'bold_italic' => $fontDir . '/Times-BoldItalic',
+  ),
+  'courier' => array(
+    'normal' => $fontDir . '/Courier',
+    'bold' => $fontDir . '/Courier-Bold',
+    'italic' => $fontDir . '/Courier-Oblique',
+    'bold_italic' => $fontDir . '/Courier-BoldOblique',
+  ),
+  'helvetica' => array(
+    'normal' => $fontDir . '/Helvetica',
+    'bold' => $fontDir . '/Helvetica-Bold',
+    'italic' => $fontDir . '/Helvetica-Oblique',
+    'bold_italic' => $fontDir . '/Helvetica-BoldOblique',
+  ),
+  'zapfdingbats' => array(
+    'normal' => $fontDir . '/ZapfDingbats',
+    'bold' => $fontDir . '/ZapfDingbats',
+    'italic' => $fontDir . '/ZapfDingbats',
+    'bold_italic' => $fontDir . '/ZapfDingbats',
+  ),
+  'symbol' => array(
+    'normal' => $fontDir . '/Symbol',
+    'bold' => $fontDir . '/Symbol',
+    'italic' => $fontDir . '/Symbol',
+    'bold_italic' => $fontDir . '/Symbol',
+  ),
+  'serif' => array(
+    'normal' => $fontDir . '/Times-Roman',
+    'bold' => $fontDir . '/Times-Bold',
+    'italic' => $fontDir . '/Times-Italic',
+    'bold_italic' => $fontDir . '/Times-BoldItalic',
+  ),
+  'monospace' => array(
+    'normal' => $fontDir . '/Courier',
+    'bold' => $fontDir . '/Courier-Bold',
+    'italic' => $fontDir . '/Courier-Oblique',
+    'bold_italic' => $fontDir . '/Courier-BoldOblique',
+  ),
+  'fixed' => array(
+    'normal' => $fontDir . '/Courier',
+    'bold' => $fontDir . '/Courier-Bold',
+    'italic' => $fontDir . '/Courier-Oblique',
+    'bold_italic' => $fontDir . '/Courier-BoldOblique',
+  ),
+  'dejavu sans' => array(
+    'bold' => $fontDir . '/DejaVuSans-Bold',
+    'bold_italic' => $fontDir . '/DejaVuSans-BoldOblique',
+    'italic' => $fontDir . '/DejaVuSans-Oblique',
+    'normal' => $fontDir . '/DejaVuSans',
+  ),
+  'dejavu sans mono' => array(
+    'bold' => $fontDir . '/DejaVuSansMono-Bold',
+    'bold_italic' => $fontDir . '/DejaVuSansMono-BoldOblique',
+    'italic' => $fontDir . '/DejaVuSansMono-Oblique',
+    'normal' => $fontDir . '/DejaVuSansMono',
+  ),
+  'dejavu serif' => array(
+    'bold' => $fontDir . '/DejaVuSerif-Bold',
+    'bold_italic' => $fontDir . '/DejaVuSerif-BoldItalic',
+    'italic' => $fontDir . '/DejaVuSerif-Italic',
+    'normal' => $fontDir . '/DejaVuSerif',
+  ),
+  'tiro devanagari hindi' => array(
+    'normal' => $fontDir . '/tiro_devanagari_hindi_normal_3aee99a8ffab1f31c32d289f3d910262',
+    'italic' => $fontDir . '/tiro_devanagari_hindi_italic_b901fd9ce651b665d81308d5ec6d6ea2',
+  ),
+  'poppins' => array(
+    'normal' => $fontDir . '/poppins_normal_442dffd12abdf0df26c14405d3f49bfa',
+  ),
+  'noto sans devanagari' => array(
+    'normal' => $fontDir . '/noto_sans_devanagari_normal_f396c01c7a196910b22cb2f53d7cf9d4',
+    'bold' => $fontDir . '/noto_sans_devanagari_bold_e90a02628d086f8080fc636f34687d7c',
+  ),
+);
+}; ?>
--- php/quiz-settings-tab-content.php
+++ php/quiz-settings-tab-content.php
@@ -31,14 +31,14 @@ function qsm_addon_certificate_quiz_settings_tabs_content() {
 
   // If nonce is set and correct, save certificate settings
   if ( isset( $_POST["certificate_nonce"] ) && wp_verify_nonce( $_POST['certificate_nonce'], 'certificate') ) {
-
     // Prepares certificate settings array
     $certificate_settings = array(
       'enabled' => intval( $_POST["enableCertificates"] ),
-      'certificate_font' => sanitize_text_field( $_POST["certificate_font"] ),
+      'certificate_size'=>  isset($_POST["certificateSize"]) ? $_POST['certificateSize'] : "Landscape" ,
+      'certificate_font' => htmlspecialchars( preg_replace( '#<script(.*?)>(.*?)</script>#is', '', sanitize_textarea_field( wp_unslash( $_POST['certificate_font'] ) ) ), ENT_QUOTES ),
       'title' => sanitize_text_field( $_POST["certificate_title"] ),
       //'content' => wp_kses_post( $_POST["certificate_template"] ),
-      'content' =>  wp_kses($_POST["certificate_template"],
+      'content' =>  wp_kses_post($_POST["certificate_template"],
          array(
            'b' => array(),
            'i' => array(),
@@ -52,44 +52,47 @@ function qsm_addon_certificate_quiz_settings_tabs_content() {
            )
          )
         ) ,
-      'logo' => esc_url_raw( $_POST["certificate_logo"] ),
-      'background' => esc_url_raw( $_POST["certificate_background"] )
+      'logo' =>  $_POST["certificate_logo"] ,
+      'logo_style' => $_POST['certificate_logo_style'],
+      'background' =>  $_POST["certificate_background"]
     );
-
     // Saves array as QSM setting and alerts the user
     $mlwQuizMasterNext->pluginHelper->update_quiz_setting( "certificate_settings", $certificate_settings );
     $mlwQuizMasterNext->alertManager->newAlert( 'Your certificate settings has been saved successfully!', 'success' );
+    $mlwQuizMasterNext->alertManager->showAlerts();
   }
 
   // Load the settings
   $certificate_settings = $mlwQuizMasterNext->pluginHelper->get_quiz_setting( "certificate_settings" );
+ 
   if ( ! is_array( $certificate_settings ) ) {
     $quiz_options = $wpdb->get_row( $wpdb->prepare( "SELECT certificate_template FROM {$wpdb->prefix}mlw_quizzes WHERE quiz_id=%d LIMIT 1", intval( $_GET["quiz_id"] ) ) );
     //Load Certificate Options Variables
   	if ( is_serialized( $quiz_options->certificate_template ) && is_array( @unserialize( $quiz_options->certificate_template ) ) ) {
   		$certificate = @unserialize( $quiz_options->certificate_template );
-      $certificate_settings = array(
-        'enabled' => $certificate[4],
-        'title' => $certificate[0],
-        'content' => $certificate[1],
-        'logo' => $certificate[2],
-        'background' => $certificate[3]
-      );
+      $certificate_settings	 = array(
+				'enabled'	 => $certificate[4],
+				'title'		 => $certificate[0],
+				'content'	 => $certificate[1],
+				'logo'		 => $certificate[2],
+				'background' => $certificate[3]
+			);
   	}
   }
   $certificate_defaults = array(
+    'certificate_size' => 'Landscape',
     'enabled' => 1,
-    'certificate_font' => 'dejavusans',
+    'certificate_font' => '',
     'title' => 'Enter your title',
     'content' => 'Enter your content',
     'logo' => '',
+    'logo_style' => '',
     'background' => ''
   );
   $certificate_settings = wp_parse_args( $certificate_settings, $certificate_defaults );
-
 	?>
   <h2><?php echo __('Certificate', 'qsm-certificate'); ?></h2>
-  <p><b><?php echo __('After enabling and configuring your certificate, you will have to add it to an email on the Emails tab or a results page on the Results Page tab using the %CERTIFICATE_LINK% variable.', 'qsm-certificate'); ?></b></p>
+  <p><b><?php echo __('After enabling and configuring. your certificate, you will have to add it to an email on the Emails tab or a results page on the Results Page tab using the %CERTIFICATE_LINK% variable.', 'qsm-certificate'); ?></b></p>
   <p><a href="https://quizandsurveymaster.com/docs/v7/adding-custom-font-to-certificate-addon/" target="_blank"><?php echo __('Click here', 'qsm-certificate') ?></a> <?php echo __('to learn about adding custom fonts.', 'qsm-certificate'); ?></p>
   <form action="" method="post">
     <button class="button-primary"><?php echo __('Save Settings', 'qsm-certificate'); ?></button>
@@ -102,21 +105,27 @@ function qsm_addon_certificate_quiz_settings_tabs_content() {
 				</td>
 			</tr>
       <tr valign="top">
+				<td><label for="certificateSize"><?php echo __('Certificate Orientation', 'qsm-certificate'); ?></label></td>
+				<td>
+				    <input type="radio" id="radio32" name="certificateSize" <?php checked( $certificate_settings["certificate_size"], 'Portrait' ); ?> value='Portrait' /><label for="radio32"><?php _e('Portrait', 'qsm-certificate'); ?></label><br>
+				    <input type="radio" id="radio33" name="certificateSize" <?php checked( $certificate_settings["certificate_size"], 'Landscape','Landscape' ); ?> value='Landscape' /><label for="radio33"><?php _e('Landscape', 'qsm-certificate'); ?></label><br>
+				</td>
+			</tr>
+      <tr valign="top">
 				<td>
           <label for="enableCertificates"><strong><?php echo __('Certificates font family', 'qsm-certificate'); ?></strong></label>
-          <p>
-            <?php echo __('Please add font file (ttf) attachment ID or file path.', 'qsm-certificate') ?>
-          </p>
+      
         </td>
 				<td>
-				    <input type="text" id="certificate_font" name="certificate_font" value='<?php echo $certificate_settings["certificate_font"]; ?>' /><br>
-				</td>
+          <textarea cols="50" rows="8" id="certificate_font" name="certificate_font"><?php echo trim( htmlspecialchars_decode( $certificate_settings["certificate_font"], ENT_QUOTES ) ); ?></textarea>
+          </td>
 			</tr>
 			<tr>
 				<td width="30%">
 					<strong><?php echo __('Title', 'qsm-certificate'); ?></strong>
 				</td>
-                                <td><textarea cols="80" rows="15" id="certificate_title" name="certificate_title"><?php echo stripslashes( $certificate_settings["title"] ); ?></textarea>
+        <td>
+          <textarea cols="80" rows="3" id="certificate_title" name="certificate_title"><?php echo stripslashes( $certificate_settings["title"] ); ?></textarea>
 				</td>
 			</tr>
 			<tr>
@@ -124,7 +133,7 @@ function qsm_addon_certificate_quiz_settings_tabs_content() {
 					<strong><?php _e('Content', 'qsm-certificate'); ?></strong>
 					<br />
 					<p><?php _e('Allowed Variables:', 'qsm-certificate'); ?></p>
-					<p style="margin: 2px 0">- %POINT_SCORE%</p>
+					<p style="margin: 2px 0" >- %POINT_SCORE%</p>
 					<p style="margin: 2px 0">- %AVERAGE_POINT%</p>
 					<p style="margin: 2px 0">- %AMOUNT_CORRECT%</p>
 					<p style="margin: 2px 0">- %TOTAL_QUESTIONS%</p>
@@ -139,23 +148,35 @@ function qsm_addon_certificate_quiz_settings_tabs_content() {
 					<p style="margin: 2px 0">- %DATE_TAKEN%</p>
           <?php do_action('qsm_certificate_after_variable'); ?>
 				</td>
-				<td><label for="certificate_template"><?php echo __('Allowed tags', 'qsm-certificate'); ?>: &lt;b&gt; - bold, &lt;i&gt;-italics, &lt;u&gt;-underline, &lt;br&gt;-<?php echo __('New Line or start a new line by pressing enter', 'qsm-certificate') ?></label><textarea cols="80" rows="15" id="certificate_template" name="certificate_template"><?php echo $certificate_settings["content"]; ?></textarea>
-				</td>
+				<td><label for="certificate_template"><?php echo __('Allowed tags', 'qsm-certificate'); ?>: &lt;b&gt; - bold, &lt;i&gt;-italics, &lt;u&gt;-underline, &lt;br&gt;-<?php echo __('New Line or start a new line by pressing enter', 'qsm-certificate') ?></label>
+				<?php wp_editor( htmlspecialchars_decode( $certificate_settings["content"], ENT_QUOTES ), 'certificate_template', array(
+												'editor_height' => 250,
+												'textarea_rows' => 10,
+											) ); ?>
+			</td>
 			</tr>
 			<tr>
 				<td width="30%">
 					<strong><?php echo __('URL To Logo (Must be JPG, JPEG, PNG, GIF or SVG)', 'qsm-certificate'); ?></strong>
 				</td>
-				<td><textarea cols="80" rows="15" id="certificate_logo" name="certificate_logo"><?php echo $certificate_settings["logo"]; ?></textarea>
+				<td><textarea cols="80" rows="3" id="certificate_logo" name="certificate_logo"><?php echo $certificate_settings["logo"]; ?></textarea>
+				</td>
+			</tr>
+      <tr>
+				<td width="30%">
+					<strong><?php echo __('Logo Img style(CSS properties)', 'qsm-certificate'); ?></strong>
+				</td>
+				<td><textarea cols="80" rows="3" id="certificate_logo_style" name="certificate_logo_style"><?php echo $certificate_settings["logo_style"]; ?></textarea>
 				</td>
 			</tr>
 			<tr>
 				<td width="30%">
 					<strong><?php echo __('URL To Background Img (Must be JPG, JPEG, PNG, GIF or SVG)', 'qsm-certificate'); ?></strong>
 				</td>
-				<td><textarea cols="80" rows="15" id="certificate_background" name="certificate_background"><?php echo $certificate_settings["background"]; ?></textarea>
+				<td><textarea cols="80" rows="3" id="certificate_background" name="certificate_background"><?php echo $certificate_settings["background"]; ?></textarea>
 				</td>
 			</tr>
+      
 		</table>
     <?php wp_nonce_field('certificate','certificate_nonce'); ?>
 		<button class="button-primary"><?php echo __('Save Settings', 'qsm-certificate'); ?></button>

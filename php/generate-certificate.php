0c24663b3dfe74a70155f7c5d3395dc720223a29
Mohammad Zubair Ali <90672776+zubairraeen@users.noreply.github.com>
2023-05-04 00:06:21 +0530

    Revert "conflict between the  certificate addon and export results addon"

--- php/generate-certificate.php
+++ php/generate-certificate.php
@@ -16,11 +16,7 @@ use Dompdf\Options;
  * @return bool|string Returns false if file fails to generate. If $return_file is false, then the function will return true if pdf generation is success. If $return_file is set to true, the function will return the file's path
  */
 function qsm_addon_certificate_generate_certificate( $quiz_results, $return_file = false ) {
-    $classes = get_declared_classes();
-    //echo "<pre>";print_r($classes);exit;
-    if( !class_exists( 'Dompdf\Autoloader' ) ) {
-        require_once(plugin_dir_path(__FILE__) . '../dompdf/autoload.inc.php');
-    }
+    require_once(plugin_dir_path(__FILE__) . '../dompdf/autoload.inc.php');
 	global $wpdb;
 	global $mlwQuizMasterNext;
 	// Load the settings.

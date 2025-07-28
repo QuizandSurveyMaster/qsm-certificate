65e7690550976b69375a7b805a3023006f675569
et-manan <manandeep@expresstech.io>
2025-07-28 17:38:34 +0530

    Fix script load issues and seperate script for frontend

--- qsm-certificate.php
+++ qsm-certificate.php
@@ -113,7 +113,6 @@ class QSM_Certificate {
         add_filter( 'qmn_email_template_variable_results', 'qsm_certificate_attach_certificate_file', 10, 2 );
 		add_filter( 'qsm_text_variable_list_email', array( $this, 'qsm_certificate_show_variable' ), 10, 1 );
 		add_filter( 'qsm_text_variable_list_result', array( $this, 'qsm_certificate_link_variable' ), 10, 1 );
-		add_filter( 'wp_editor_settings', 'qsm_certificate_preview_allow_br_tags' );
 	}
 
 	/**
